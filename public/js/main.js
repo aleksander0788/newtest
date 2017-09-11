683f8df667b6438d027c30c9c7b74c393ae6ecca
Newyorrker <newyorrker@gmail.com>
2017-09-11 21:10:20 +0300

    fixed some bugs

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-
+console.log('hi there');
 var li = $('.client-nav__list-item');
 
 li.hover(
 	function (e) {
-
+		console.log('hi there');
 		$(this).children('.client-nav__dropdown').toggle();
 		$(this).children('span').toggle();
 
