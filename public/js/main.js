ae133996c7fbb5a2a535cdd9a1fb30b70f68228d
Newyorrker <newyorrker@gmail.com>
2017-09-11 22:30:55 +0300

    added carousel slick

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
-console.log('hi there');
-var li = $('.client-nav__list-item');
+	var li = $('.client-nav__list-item');
 
-li.hover(
-	function (e) {
-		console.log('hi there');
-		$(this).children('.client-nav__dropdown').toggle();
-		$(this).children('span').toggle();
+	li.hover(
+		function (e) {
+			console.log('hi there');
+			$(this).children('.client-nav__dropdown').toggle();
+			$(this).children('span').toggle();
 
-	})
+		});
+
+	$('.partners__carousel').slick({
+    infinite: true,
+	  slidesToShow: 4,
+	  slidesToScroll: 3,
+	  prevArrow: '<span class="slick-arrow"></span>',
+	  nextArrow: '<span class="slick-arrow slick-next"></span>'
+
+  });
+
 });
